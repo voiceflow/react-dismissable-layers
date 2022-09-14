c7ef951e699442348fbf82b1ecb1b6c1b52997ec
Yauheni <overhawlin@gmail.com>
2022-09-14 16:57:36 +0200

    Updates

---
+++ assets/es.regexp.flags.6809c0d1.js
@@ -0,0 +1,2 @@
+import{aa as e,a8 as r,a_ as t,al as a}from"./iframe.4b9c4b64.js";var o=e,s=r,p=t,l=a,g=o&&l(function(){return Object.getOwnPropertyDescriptor(RegExp.prototype,"flags").get.call({dotAll:!0,sticky:!0})!=="sy"});g&&s.f(RegExp.prototype,"flags",{configurable:!0,get:p});
+//# sourceMappingURL=es.regexp.flags.6809c0d1.js.map
